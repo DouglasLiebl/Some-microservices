6eb569b4bddc4ef54045fe2d94bb4b2854208d1e
Douglas Liebl <douglasliebl@outlook.com>
2023-10-04 12:06:54 -0300

    Adding gateway

---
+++ api-gateway/src/main/java/io/github/douglasliebl/apigateway/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package io.github.douglasliebl.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/product/**").uri("lb://ms-products"))
+                .route(r -> r.path("/manufacturer/**").uri("lb://ms-products"))
+                .route(r -> r.path("/category/**").uri("lb://ms-products"))
+                .build();
+    }
+}
